531b6178b5bff67c18fda13105379b0f3d8c4c1b
Rory <rorystock06@gmail.com>
2024-08-23 14:02:48 +1200

    Download image works
    All main features working

--- site/app/Http/Controllers/ImageController.php
+++ site/app/Http/Controllers/ImageController.php
@@ -11,12 +11,10 @@ class ImageController extends Controller
 {
     public function downloadR2Image($folder, $fileName)
     {
-        $folder = str_replace('%20', ' ', $folder);
-
         // Get the file from the R2 storage disk
         $disk = Storage::disk('r2');
         $directory = '/'. $folder . '/Low Res/';
-        $file = $disk->get($directory . $fileName);
+        $file = $disk->get($directory . $fileName . '.jpg');
 
         // Download the file
         return response()->streamDownload(function () use ($file) {
